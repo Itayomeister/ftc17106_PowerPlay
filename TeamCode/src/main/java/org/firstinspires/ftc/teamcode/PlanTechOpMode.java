e390ac9dadf7e9996c0a7202a2187f3ec52c98c3
itayo <itay.tamari@gmail.com>
2022-11-10 19:23:12 +0200

    CameraVision branch

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PlanTechOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PlanTechOpMode.java
@@ -35,12 +35,6 @@ import org.openftc.easyopencv.OpenCvWebcam;*/
 
 public abstract class PlanTechOpMode extends LinearOpMode {
 
-    DcMotorEx arm1, arm2;
-    int countsPreRevolution = 28;
-    double armGearBox = 143.1188; // 5:5:5:1
-    final int armCountsPerRev = (int) (armGearBox * countsPreRevolution);
-    final int FINALARMCOUNT = (int) (.25 * armCountsPerRev);
-
     Gamepad gamepad1, gamepad2;
 
     protected ElapsedTime runtime = new ElapsedTime();
@@ -73,50 +67,6 @@ public abstract class PlanTechOpMode extends LinearOpMode {
 
     protected abstract void end();
 
-
-    public void armHigh() {
-        double degreees = toDegrees(asin(70 / 80));
-        //B = beta,degreees = alpha,T=ticks
-        int B = (int) (90 - degreees);
-        int BtoY = 90 / B;
-        int YtoT = FINALARMCOUNT / BtoY;
-        arm1.setTargetPosition(YtoT);
-        arm2.setTargetPosition((int) .5 * FINALARMCOUNT);
-    }
-
-    public void armMid() {
-        double degreees = toDegrees(asin(40 / 80));//30 deg
-        //B = beta,degreees = alpha,T=ticks
-        int B = (int) (90 - degreees);//60
-        int BtoY = 90 / B;//1.5
-        int YtoT = FINALARMCOUNT / BtoY;
-        arm1.setTargetPosition(YtoT);
-        arm2.setTargetPosition((int) .5 * FINALARMCOUNT);
-    }
-
-    public void armlow() {
-        double degreees = toDegrees(asin(20 / 80));
-        //B = beta,degreees = alpha,T=ticks
-        int B = (int) (90 - degreees);
-        int BtoY = 90 / B;
-        int YtoT = FINALARMCOUNT / BtoY;
-        arm1.setTargetPosition(YtoT);
-        arm2.setTargetPosition((int) .5 * FINALARMCOUNT);
-    }
-
-    public void armgound() {
-        int alpha = (int) toDegrees(asin(30 / 40));
-        int beta = 90 - alpha;
-        int alphaToTicks = alpha / 360 * FINALARMCOUNT;
-        int betaToTicks = beta / 360 * FINALARMCOUNT;
-        arm1.setTargetPosition(alphaToTicks);
-        arm2.setTargetPosition(betaToTicks);
-    }
-
-    public void armHigh2() {
-        int degreesToTicks = FINALARMCOUNT *135/360; // sets to 135 deg angle
-        arm2.setTargetPosition(degreesToTicks);
-    }
 }
 
 
